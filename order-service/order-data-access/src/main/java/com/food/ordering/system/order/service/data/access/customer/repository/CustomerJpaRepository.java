a44b16a9ba210851c381ecc8b128d1d099e648d7
dogaanismail <ismaildogaan@gmail.com>
2022-06-11 00:08:46 +0300

    food-ordering-system: Implementation of data accessing for customer and restaurant has been completed.

---
+++ order-service/order-data-access/src/main/java/com/food/ordering/system/order/service/data/access/customer/repository/CustomerJpaRepository.java
@@ -0,0 +1,11 @@
+package com.food.ordering.system.order.service.data.access.customer.repository;
+
+import com.food.ordering.system.order.service.data.access.customer.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerJpaRepository extends JpaRepository<CustomerEntity, UUID> {
+}

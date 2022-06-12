40d023cf556a1f3e344f3bb43d6678257baf4e3c
dogaanismail <ismaildogaan@gmail.com>
2022-06-13 01:23:15 +0300

    `food-ordering-system: Implementation of restaurant service container module has been done.

--- restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/BeanConfiguration.java
+++ restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/BeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.domain;
+package com.food.ordering.system.restaurant.service.domain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeanConfiguration {
     @Bean
-    public OrderDomainService orderDomainService(){
-        return new OrderDomainServiceImpl();
+    public RestaurantDomainService restaurantDomainService() {
+        return new RestaurantDomainServiceImpl();
     }
 }
